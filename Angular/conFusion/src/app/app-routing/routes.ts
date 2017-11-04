01080659a08b71a7267b005d4c6741c80e7b7124
Shane Honanie <shaneh425@gmail.com>
2017-11-04 15:11:47 -0700

    Route Paramters Added

--- Angular/conFusion/src/app/app-routing/routes.ts
+++ Angular/conFusion/src/app/app-routing/routes.ts
@@ -7,7 +7,9 @@ import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
-    { path: 'home',  component: HomeComponent },
-    { path: 'menu',     component: MenuComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'dishdetail/:id', component: DishdetailComponent },
+    { path: 'contactus', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
   ];
--- Angular/conFusion/src/app/services/dish.service.ts
+++ Angular/conFusion/src/app/services/dish.service.ts
@@ -5,9 +5,17 @@ import { DISHES } from '../shared/dishes';
 @Injectable()
 export class DishService {
 
-  constructor() { }
+	constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
-  }
+	getDishes(): Dish[] {
+		return DISHES;
+    }
+
+	getDish(id: number): Dish {
+    	return DISHES.filter((dish) => (dish.id === id))[0];
+    }
+
+	getFeaturedDish(): Dish {
+		return DISHES.filter((dish) => dish.featured)[0];
+	}
 }
