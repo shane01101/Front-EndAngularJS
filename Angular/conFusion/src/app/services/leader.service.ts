e1e3e5f807af80cca881f6fa3bee3fd3f0af5d1c
Shane Honanie <shaneh425@gmail.com>
2017-11-07 01:02:05 -0800

    Promises added to services and handled in components

--- Angular/conFusion/src/app/services/leader.service.ts
+++ Angular/conFusion/src/app/services/leader.service.ts
@@ -7,16 +7,16 @@ export class LeaderService {
 
 	constructor() { }
 
-	getLeaders(): Leader[] {
-		return LEADERS;
+	getLeaders(): Promise<Leader[]> {
+		return Promise.resolve(LEADERS);
 	}
 
-	getLeader(id: number): Leader {
-		return LEADERS.filter((ld) => (ld.id === id))[0];
+	getLeader(id: number): Promise<Leader> {
+		return Promise.resolve(LEADERS.filter((ldr) => (ldr.id === id))[0]);
 	}
 
-	getFeaturedLeader(): Leader {
-		return LEADERS.filter((ld) => ld.featured)[0];
+	getFeaturedLeader(): Promise<Leader> {
+		return Promise.resolve(LEADERS.filter((ldr) => ldr.featured)[0]);
 	}
 
 }
