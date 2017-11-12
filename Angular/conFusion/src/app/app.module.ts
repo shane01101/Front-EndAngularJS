9ede44c1424e048c1b424e3932c3571082c72457
Shane Honanie <shaneh425@gmail.com>
2017-11-12 02:07:57 -0800

    Animations Pt 1

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdSliderModule } from '@angular/material/typings/slider';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -15,7 +16,20 @@ import 'rxjs/add/operator/switchMap';
 @Component({
 	selector: 'app-dishdetail',
 	templateUrl: './dishdetail.component.html',
-	styleUrls: ['./dishdetail.component.scss']
+	styleUrls: ['./dishdetail.component.scss'],
+	animations: [
+		trigger('visibility', [
+			state('shown', style({
+				transform: 'scale(1.0)',
+				opacity: 1
+			})),
+			state('hidden', style({
+				transform: 'scale(0.5)',
+				opacity: 0
+			})),
+			transition('* => *', animate('0.5s ease-in-out'))
+		])
+	]
 })
 export class DishdetailComponent implements OnInit {
 	dish: Dish;
@@ -27,6 +41,7 @@ export class DishdetailComponent implements OnInit {
 	commentForm: FormGroup;
 	comment: Comment;
 	rating: Number;
+	visibility = 'shown';
 
 	formErrors = {
 		'author': '',
@@ -35,11 +50,11 @@ export class DishdetailComponent implements OnInit {
 
 	validationMessages = {
 		'author': {
-			'required':      'Author name is required.',
-			'minlength':     'Author Name must be at least 2 chars long.'
+			'required': 'Author name is required.',
+			'minlength': 'Author Name must be at least 2 chars long.'
 		},
 		'comment': {
-			'required':      'Comment is required.'
+			'required': 'Comment is required.'
 		}
 	};
 
@@ -56,13 +71,8 @@ export class DishdetailComponent implements OnInit {
 			.subscribe(dishIds => this.dishIds = dishIds);
 
 		this.route.params
-			.switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-			.subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id)},
-			errmess => this.errMess = <any>errmess);
-
-		this.route.params
-			.switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
-			.subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+			.switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+			.subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
 				errmess => { this.dish = null; this.errMess = <any>errmess; });
 	}
 
