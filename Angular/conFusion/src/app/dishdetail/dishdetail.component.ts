4d2f362448c7bae741ce88612f4179ed9655e208
Shane Honanie <shaneh425@gmail.com>
2017-11-15 15:15:31 -0800

    Animations Pt 2

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,7 +4,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdSliderModule } from '@angular/material/typings/slider';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -17,18 +17,14 @@ import 'rxjs/add/operator/switchMap';
 	selector: 'app-dishdetail',
 	templateUrl: './dishdetail.component.html',
 	styleUrls: ['./dishdetail.component.scss'],
+	host: {
+		'[@flyInOut]' : 'true',
+		'style': 'display: block;'
+	},
 	animations: [
-		trigger('visibility', [
-			state('shown', style({
-				transform: 'scale(1.0)',
-				opacity: 1
-			})),
-			state('hidden', style({
-				transform: 'scale(0.5)',
-				opacity: 0
-			})),
-			transition('* => *', animate('0.5s ease-in-out'))
-		])
+		flyInOut(), 
+		visibility(),
+		expand()
 	]
 })
 export class DishdetailComponent implements OnInit {
